b3d48458b0d20367e0ccb53ee1872b98ce589df3
管丽鹏 <1965772648@qq.com>
2022-10-07 23:40:55 +0800

    [feat]:1.调整代码逻辑，增加爬取单个详情页的功能;2.优化注释

--- spiders/image_spider.py
+++ spiders/image_spider.py
@@ -1,33 +1,50 @@
-import time
+"""
+抓取内容：
+  AV_id  image_urls
+
+运行：
+  scrapy crawl image
+"""
 import scrapy
 
 from spider_netflav.items import SpiderNetflavImageItem
 
-'''
-抓取内容：
-  AV_id  image_urls
 
-运行：(setting中记得修改配置)
-  scrapy crawl image(爬虫名)
-'''
-
-
+# TODO: setting中记得修改配置
 class ImageSpider(scrapy.Spider):
     # TODO: 是否使用测试模式（仅爬取少量数据）
     DEBUG = False
     # 爬虫名字
     name = 'image'
-    # 列表页链接(&page不要放进去)
-    start_urls = ['https://netflav.com/all?actress=%E9%88%B4%E5%8E%9F%E3%82%A8%E3%83%9F%E3%83%AA']
     # 图片下载的前缀链接
     prefix_url = 'https://pics.dmm.co.jp/digital/video/'
-    # 开始页数
-    page = 1
-    # 总页数
-    MAX_PAGE = 4
     
-    # @override 默认开始函数
+    # 单个详情页的链接
+    detail_urls = [
+    ]
+    # 列表页链接(&page不要放进去)
+    index_url = ''
+    # 列表页开始页数
+    page = 1
+    # 列表页总页数
+    END_PAGE = 1
+    
+    def start_requests(self):
+        """@override \n
+        此函数将优先调用，可在此处进行初始化或选择不同的解析函数
+        """
+        # 单独详情页爬取
+        if len(self.detail_urls) > 0:
+            for itemUrl in self.detail_urls:
+                yield scrapy.Request(url=itemUrl, callback=self.parse_detail, meta={'detail_href': itemUrl})
+        # 索引页爬取
+        if len(self.index_url) > 0:
+            yield scrapy.Request(url=self.index_url, callback=self.parse)
+    
     def parse(self, response, **kwargs):
+        """@override \n
+        默认解析函数，此函数对列表页进行了解析，并进一步调用parse_detail()函数
+        """
         for div in response.xpath('//div[@class="grid_cell"]'):  # 单个电影的div结构
             detail_href = 'https://netflav.com' + div.xpath('.//a[1]/@href').get()  # 得到单个电影的详情链接
             # 判断当前div是否有图片
@@ -36,19 +53,20 @@ class ImageSpider(scrapy.Spider):
             if have_img:
                 yield scrapy.Request(detail_href, callback=self.parse_detail, meta={'detail_href': detail_href})
             if self.DEBUG: break
-            # time.sleep(1)  # 请求间隔
         
         # 翻页后继续解析
         if self.DEBUG: return
-        if self.page < self.MAX_PAGE:
+        if self.page < self.END_PAGE:
             self.page += 1
-            next_url = self.start_urls[0] + '&page=' + str(self.page)
-            print(f'Now page = {self.page}, url = {next_url}')
+            next_url = self.index_url + '&page=' + str(self.page)
+            print(f'Now page= {self.page}, url= {next_url}')
             yield scrapy.Request(next_url, callback=self.parse)  # 调用parse()函数爬取下一页
     
-    # 爬取详情页
-    # 思路：通过番号拼凑出下载图片的完整链接
     def parse_detail(self, response):
+        """
+        爬取详情页，并存入item \n
+        思路：通过番号拼凑出下载图片的完整链接
+        """
         cover_url: str = response.xpath('//meta[@property="og:image"]/@content').get()
         image_urls = [cover_url]
         suffix = cover_url.split('/')[-2]  # 示例：http.../118raw00006/118raw00006pl.jpg -> 118raw00006
@@ -60,5 +78,6 @@ class ImageSpider(scrapy.Spider):
         item = SpiderNetflavImageItem()
         item['AV_id'] = response.xpath('//*[@id="video-details"]/div[4]/div[1]/div[2]/text()').get()
         item['image_urls'] = image_urls
-        print(f"{suffix} success, image len={len(image_urls)}, detail_href={response.meta['detail_href']}")  # 打印结果
+        # 打印结果
+        print(f"{item['AV_id']} success, image numbers= {len(image_urls)}, detail_href= {response.meta['detail_href']}")
         yield item
--- spiders/text_spider.py
+++ spiders/text_spider.py
@@ -1,30 +1,48 @@
-import time
-import scrapy
-from spider_netflav.items import SpiderNetflavItem
-
-'''
+"""
 抓取内容：
   番号(AV_id)  发行日期(AV_date)  番名(AV_title)  类别(AV_sort)
- 
-运行：(setting中记得修改配置)
-  scrapy crawl text(爬虫名) -o text.json
-'''
+
+运行：
+  scrapy crawl text -o text.csv
+"""
+import scrapy
+
+from spider_netflav.items import SpiderNetflavItem
 
 
+# TODO: setting中记得修改配置
 class TextSpider(scrapy.Spider):
     # TODO: 是否使用测试模式（仅爬取少量数据）
     DEBUG = False
     # 爬虫名字
     name = 'text'
-    # 列表页链接
-    start_urls = ['https://netflav.com/all?actress=%E9%88%B4%E5%8E%9F%E3%82%A8%E3%83%9F%E3%83%AA']
-    # 开始页数
-    page = 1
-    # 总页数
-    MAX_PAGE = 4
     
-    # @override 默认开始函数
+    # 单个详情页的链接
+    detail_urls = [
+    ]
+    # 列表页链接(&page不要放进去)
+    index_url = ''
+    # 列表页开始页数
+    page = 1
+    # 列表页总页数
+    END_PAGE = 1
+    
+    def start_requests(self):
+        """@override \n
+        此函数将优先调用，可在此处进行初始化或选择不同的解析函数
+        """
+        # 单独详情页爬取
+        if len(self.detail_urls) > 0:
+            for itemUrl in self.detail_urls:
+                yield scrapy.Request(url=itemUrl, callback=self.parse_detail, meta={'detail_href': itemUrl})
+        # 索引页爬取
+        if len(self.index_url) > 0:
+            yield scrapy.Request(url=self.index_url, callback=self.parse)
+    
     def parse(self, response, **kwargs):
+        """@override \n
+        默认解析函数，此函数对列表页进行了解析，并进一步调用parse_detail()函数
+        """
         for div in response.xpath('//div[@class="grid_cell"]'):  # 单个电影的div结构
             detail_href = 'https://netflav.com' + div.xpath('.//a[1]/@href').get()  # 得到单个电影的详情链接
             yield scrapy.Request(detail_href, callback=self.parse_detail, meta={'detail_href': detail_href})
@@ -33,22 +51,26 @@ class TextSpider(scrapy.Spider):
         
         # 翻页后继续解析
         if self.DEBUG: return
-        if self.page < self.MAX_PAGE:
+        if self.page < self.END_PAGE:
             self.page += 1
-            next_url = self.start_urls[0] + '&page=' + str(self.page)
-            print(f'Now page = {self.page}, url = {next_url}')
+            next_url = self.index_url + '&page=' + str(self.page)
+            print(f'Now page= {self.page}, url= {next_url}')
             yield scrapy.Request(next_url, callback=self.parse)  # 调用parse()函数爬取下一页
     
-    # 爬取详情页
     def parse_detail(self, response):
+        """
+        爬取详情页，并存入item
+        """
         root_div = response.xpath('//div[@class="videodetail_root"]')
         item = SpiderNetflavItem()
         item['AV_id'] = root_div.xpath('./div[4]/div[1]/div[2]/text()').get()  # 提取出的内容：ADN-242
         item['AV_date'] = root_div.xpath('./div[4]/div[2]/div[2]/text()').get()  # 提取出的内容：2019-01-13
         item['AV_title'] = root_div.xpath('./div[1]/text()').get()
-        temp = []
+        sort = ""
         for string in root_div.xpath('./div[4]/div[4]//text()').getall():
-            if (',' not in string) and (':' not in string): temp.append(string)
-        item['AV_sort'] = temp
-        print(f"{item['AV_id']} is success, detail_href = {response.meta['detail_href']}")
+            if (',' not in string) and (':' not in string) and ('類別' not in string):
+                if len(sort) > 0: sort += '，'
+                sort += string
+        item['AV_sort'] = sort
+        print(f"{item['AV_id']} is success, detail_href= {response.meta['detail_href']}")
         yield item
